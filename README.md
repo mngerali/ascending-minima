52b383c9ee6d406416831239247b21267fecfc7d
MnGer <mngerali@gmail.com>
2020-02-02 01:31:59 +0200

    Updated README.md and MainClass.java files
    Added extra comments in MainClass.java and README.md files.

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# ascending-minima
-Repository containing custom solution related to Ascending Minima algorithm
+This project includes the below classes:
+- Class `StatisticUtilsArray`: provides a set of statistical values (min,
+max, median, mean and std) on an array of doubles.
+- Class `StatisticUtilsArrayTest`: performs testing of the functions implemented
+within StatisticUtilsArray class
+- Class `StatisticUtilsArrayList`: provides a set of statistical values (min,
+max, median, mean and std) on an array list of doubles
+- Class `StatisticUtilsArrayListTest`: performs testing of the functions
+implemented within StatisticUtilsArrayList class
+- Class `AscendingMinima`: implements the Ascending Minima algorithm.
+Given a set of numbers, A, and a window, W, the algorithm finds the minimum
+value across the first W elements of the given set and continues with the rest
+values of that window coming after that minimum. The process of finding the
+current minimum across window in scope and adding it to the ascending minima
+list is repeated in a similar way, until all values of current window have been
+evaluated. At this point, an initial list of minimum values has been
+generated.
+The algorithm then examines next window, which is the previous rolled by one
+element. The rejected element is compared with the first element of ascending
+minima and is being removed from the latter in case they are equal. The incoming
+element is being included in the ascending minima list and is being compared
+with the remaining values of ascending minima. Any elements of ascending minima
+that are greater than the newly added one are being removed from the list.
+This process is repeated until all upcoming elements have been parsed.
+- Class `AscendingMinimaTest`: performs testing of the aforementioned Algorithm.
+- Class `MainClass`: Wrapper class that creates object of each class and prints
+the produced results.
--- src/main/java/Assignment/MainClass.java
+++ src/main/java/Assignment/MainClass.java
@@ -9,8 +9,15 @@ import java.util.*;
 public class MainClass {
     public static void main(String[] args) {
 
+        /**
+         * Initializes an array of doubles
+         */
         double[] numbers = {1, 2, 3, 4, 5, 6};
         StatisticUtilsArray stArr = new StatisticUtilsArray(numbers);
+
+        /**
+         * Prints the statistical values of the array
+         */
         System.out.println("Array Stats:");
         System.out.println("Min value: " + stArr.getMin());
         System.out.println("Max value: " + stArr.getMax());
@@ -19,10 +26,16 @@ public class MainClass {
         System.out.println("Std value: " + stArr.getStd());
 
 
+        /**
+         * Initializes an array list of doubles
+         */
         ArrayList<Double> values = new ArrayList<Double>() {
             {add(1.0); add(2.0); add(3.0);add(4.0); add(5.0); add(6.0);}
         };
 
+        /**
+         * Prints the statistical values of the array
+         */
         StatisticUtilsArrayList stArrL = new StatisticUtilsArrayList(values);
         System.out.println("ArrayList Stats:");
         System.out.println("Min value: " + stArrL.getMin());
@@ -31,12 +44,18 @@ public class MainClass {
         System.out.println("Mean value: " + stArrL.getMean());
         System.out.println("Std value: " + stArrL.getStd());
 
-
-        System.out.println("Ascending Minima algorithm");
+        /**
+         * Initializes an array list of doubles
+         */
         ArrayList<Double> al = new ArrayList<Double>();
-        List<Double> doubleList = Arrays.asList(1.0,3.0,3.0,2.0,5.0,8.0,7.0,8.0,9.0,0.0);
+        List<Double> doubleList = Arrays.asList(6.0,2.0,1.0,5.0,3.0,4.0,8.0,7.0);
         al.addAll(doubleList);
-        AscendingMinima asm = new AscendingMinima(al, 9);
+
+        /**
+         * Calculates the Ascending Minima of the above values
+         */
+        AscendingMinima asm = new AscendingMinima(al, 7);
         ArrayList<Double> minima = asm.findAscMinima();
+        System.out.println("Ascending Minima: " + minima);
     }
 }
